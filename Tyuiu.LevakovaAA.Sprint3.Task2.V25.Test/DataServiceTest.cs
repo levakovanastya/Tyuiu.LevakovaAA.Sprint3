78a5393b9b67e9806ad4212ca60966d464501540
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-10-27 16:21:49 +0500

    Task2

---
+++ Tyuiu.LevakovaAA.Sprint3.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LevakovaAA.Sprint3.Task2.V25.Lib;
+using System;
+namespace Tyuiu.LevakovaAA.Sprint3.Task2.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 13;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 16.016;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.LevakovaAA.Sprint3.Task2.V25/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LevakovaAA.Sprint3.Task2.V25.Lib;
+namespace Tyuiu.LevakovaAA.Sprint3.Task2.V25
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Левакова А.А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла do-while                                                 *");
+            Console.WriteLine("* Задание 2                                                               *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Левакова А.А. | СМАРТб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Написать программу используя цикл do-while, которая вычисляет           *");
+            Console.WriteLine("* сумму ряда по формуле, при n=5                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 13;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Стaрт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
