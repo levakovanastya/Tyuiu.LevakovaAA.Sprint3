04eb4432d24f1289be07f8df02b5288b3cd19919
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-10-29 22:58:37 +0500

    Task7

---
+++ Tyuiu.LevakovaAA.Sprint3.Task7.V11.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LevakovaAA.Sprint3.Task7.V11.Lib;
+using System;
+namespace Tyuiu.LevakovaAA.Sprint3.Task7.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+            double[] valueWairArray;
+            valueWairArray = new double[len];
+            valueWairArray[0] = 20.36;
+            valueWairArray[1] = 15.42;
+            valueWairArray[2] = 10.99;
+            valueWairArray[3] = 7.43;
+            valueWairArray[4] = 4.33;
+            valueWairArray[5] = 1;
+            valueWairArray[6] = 0;
+            valueWairArray[7] = -8.87;
+            valueWairArray[8] = -13.03;
+            valueWairArray[9] = -16.53;
+            valueWairArray[10] = -19.60;
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWairArray, res);
+        }
+    }
+}
---
+++ Tyuiu.LevakovaAA.Sprint3.Task7.V11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LevakovaAA.Sprint3.Task7.V11.Lib;
+namespace Tyuiu.LevakovaAA.Sprint3.Task7.V11
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Левакова А.А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Обработка целочисленной информации                                      *");
+            Console.WriteLine("* Задание 7                                                               *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Левакова А.А. | СМАРТб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции f(x)       *");
+            Console.WriteLine("* на заданном диапазоне [-5, 5] с шагом 1. Произвести проверку деления    *");
+            Console.WriteLine("* на 0. При делении на ноль вернуть значение 0. Значения занести в массив *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+            for (int i  = 0; i < len; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |   {1, 5:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
