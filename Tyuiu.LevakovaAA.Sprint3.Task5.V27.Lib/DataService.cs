c94ccc732aecafec280ede4e2a26594bc9178f99
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-10-29 20:31:23 +0500

    Task5

---
+++ Tyuiu.LevakovaAA.Sprint3.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.LevakovaAA.Sprint3.Task5.V27.Lib
+{
+    public class DataService : ISprint3Task5V27
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, k;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries = sumSeries + Math.Pow(k, x) / Math.Cos(k);
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
